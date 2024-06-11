34155268d373b607307303dc9c68c4f61d418f29
Matviyyyyyy <127428009+Matviyyyyyy@users.noreply.github.com>
2024-06-11 17:48:58 +0300

    Add all files

---
+++ main.py
@@ -0,0 +1,43 @@
+import django_setup
+from games_genres.models import Game, Genre
+
+# # Створення та збереження екземпляра жанру
+# genre1 = Genre(
+#     name="Тактичний шутер"
+# )
+# genre1.save()
+#
+# # Створення та збереження екземпляра гри
+# game1 = Game(
+#     title="Valorant",
+#     year_release=2020,
+#     rating=70.00
+# )
+# game1.save()
+#
+# game2 = Game(
+#     title="CSGO",
+#     year_release=2010,
+#     rating=90.00
+# )
+# # game2.save()
+#
+# # Призначення жанру для гри
+# game1.genre.add(genre1)
+# game2.genre.add(genre1)
+
+
+# genre2 = Genre(
+#     name = "Пригодницький"
+# )
+# genre2.save()
+#
+# game3 = Game(
+#     title = "Minecraft",
+#     year_release = 2009,
+#     rating=99.01
+# )
+# game3.save()
+#
+# game3.genre.add(genre2)
+
